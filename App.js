adcf3f1db1830a44ccef7a8c88992c2e49341243
khiemnguyentrong <nguyentrongkhiem.97@gmail.com>
2019-02-04 01:06:02 +0700

    practice react native basic

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, { Component } from 'react';
+import Main from './app/components/Main'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main />
+    );
+  }
+}
